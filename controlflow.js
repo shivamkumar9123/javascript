f7909654717abb1ad35cfbb48e9d4fbb6fbc88fc
shivam kumar <shivamkumarmehta9123@gmail.com>
2026-03-19 18:50:16 +0530

    control

--- controlflow.js
+++ controlflow.js
@@ -6,14 +6,45 @@
 
 // }
 //< ,>,<=,>=,==,!= ,===
-const isUserloggedIn = true
-if("2"==="2"){
-    console.log("executed");
-}
-const temperature=41
-if(temperature === 41){
-    console.log("less than 50");
-}
-else{
-console.log("temperature is greater than 50");
+// const isUserloggedIn = true
+// if("2"==="2"){
+//     console.log("executed");
+// }
+// const temperature=41
+// if(temperature === 41){
+//     console.log("less than 50");
+// }
+// else{
+// console.log("temperature is greater than 50");
+// }
+// const score=200
+// if(score>100){
+//     var power="fly"
+//     console.log(`user power:${power}`)
+// }
+// console.log(`user power:${power}`)
+// const balance=1000
+// if(balance >250)console.log("test"),console.log("test2"); //implict scope that means 
+// if(balance <500){
+//     console.log("less than 500");
+    
+// }
+// else if(balance <750){
+//     console.log("less than 750");
+    
+// }
+// else if(balance <900){
+//     console.log("less than 900");
+    
+// }
+// else{
+//     console.log("equal to 1000");
+    
+// }
+const isUserloggedIn=true
+const isUseremail=true
+const debitCard=true 
+if(isUseremail && isUserloggedIn&&debitCard){
+    console.log("allow to buy product");
+    
 }
